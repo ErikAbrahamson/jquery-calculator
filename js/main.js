5e60afef55d2c4b04ba472b1ad288ed8eb5416f9
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-30 15:36:12 -0600

    Solve JS calc

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('span').on('click', function(event) {
+      event.preventDefault();
+      if ($(this).is('#cancel')) {
+        $('#screen').text(null);
+      } else if ($(this).is('#calc')) {
+        $('#screen').text(eval($('#screen').text()));
+      } else {
+        $('#screen').append($(this).text().replace('x','*').replace('\u00f7','/'));
+      }
+   });
+});
+
+
+

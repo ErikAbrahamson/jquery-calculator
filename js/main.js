7083d18f0e6bdd703fe01ec139e9285219be64cf
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-30 21:24:01 -0600

    Indentation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $('span').on('click', function(event) {
-      event.preventDefault();
-      if ($(this).is('#cancel')) {
-        $('#screen').text(null);
-      } else if ($(this).is('#calc')) {
+  $('span').on('click', function(event) {
+    event.preventDefault();
+    if ($(this).is('#cancel')) {
+      $('#screen').text(null);
+    } else if ($(this).is('#calc')) {
         $('#screen').text(eval($('#screen').text()));
       } else {
         $('#screen').append($(this).text().replace('x','*').replace('\u00f7','/'));
